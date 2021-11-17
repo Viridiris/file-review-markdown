8711edeb0689e39d71cdc29fe386f4e8824cdf90
josephbetbeze <josephbetbeze@gmail.com>
2021-11-17 00:40:18 -0500

    markdown-template.php added programmatic population of routes table.

--- markdown-template.php
+++ markdown-template.php
@@ -10,5 +10,15 @@ function helper1(){
     $table_string .= "| Verb | Route  | Invoked Method  |<br>";
     $table_string .= "| ---- | ------ | --------------- |<br>";
 
+    $route_array_original = [
+        'example/route' => 'example_funtion',
+        'example/route2' => 'example_funtion2'
+    ];
+
+    foreach ($route_array_original as $route_uri => $function_name) {
+        $table_string .= "|  | `".$route_uri."` | `". $function_name ."()` |<br>";
+    }
+    echo $table_string;
+    echo "<br>";
 
 }
