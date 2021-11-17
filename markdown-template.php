32379b2d23ea8062579546ad4b8b1cbfc2c4a64c
josephbetbeze <josephbetbeze@gmail.com>
2021-11-17 00:38:22 -0500

    Added the markdown-template.php file, added the helper1 function, echo the file title

---
+++ markdown-template.php
@@ -0,0 +1,9 @@
+<?php
+function helper1(){
+
+    // USE THIS TO BUILD THE ROUTES TABLE IN YOUR TYPICAL MARKDOWN FILE BY COPY PASTING THE ROUTES METHOD ARRAY.
+    $file_title = "Example File Title";
+    echo "# " . $file_title . "<br><br>";
+
+
+}
